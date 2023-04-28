666094ebf9cae236e963d537c691a52624ef8f35
Richard Todd <rwtodd@users.noreply.github.com>
2023-04-28 11:06:06 -0500

    from objects to simple tables

--- wikitext-helpers/nextprev.lua
+++ wikitext-helpers/nextprev.lua
@@ -5,23 +5,7 @@ local full_title = short_title
 local book = full_title .. " (Simon)"
 local extra_categories = { }
 
--- An object for entries....
-local LSEntry = {}
-
-function LSEntry:short()
-  return self.short_entry or self.entry
-end
-
-function LSEntry:long()
-  return self.entry
-end
-
-function LSEntry:describe()
-  return self.name
-end
-
-local mt = { __index = LSEntry }
-
+-- we need ways to shorten text in a generic way, if the TOC entry is too long
 function shorten_text(t)
   -- the user may have given a starting mark...
   local shorter = t:gsub("^The ",""):gsub("^An? ","")
@@ -39,19 +23,18 @@ function make_lsentry(text)
      long_text = text:sub(1,mstart - 1)
      short_text = text:sub(mend + 1)
   end
-  local obj = { name = long_text, entry = string.format("[[%s (%s)|%s]]",long_text,short_title,long_text) }
   if #short_text > 17 then
-    local stext = shorten_text(short_text)
-    obj.short_entry = string.format("[[%s (%s)|%s]]",long_text,short_title,stext)
-  elseif short_text ~= long_text then
-    obj.short_entry = string.format("[[%s (%s)|%s]]",long_text,short_title,short_text)
-  end 
-  return setmetatable(obj,mt)
+     short_text = shorten_text(short_text)
+  end
+  return {
+    name = long_text,
+    short = string.format("[[%s (%s)|%s]]",long_text,short_title,short_text),
+    long = string.format("[[%s (%s)|%s]]",long_text,short_title,long_text)
+  }
 end
 
 function make_raw_lsentry(name, text)
-  local obj = { entry = text, name = name }
-  return setmetatable(obj,mt)  
+  return { name = name, long = text, short = text }
 end
 
 local nav = book .. " Nav"
@@ -60,25 +43,25 @@ local toc = { make_raw_lsentry("Contents", "[[:Category:" .. book .. "|Contents]
 for line in io.lines() do
   table.insert(toc, make_lsentry(line))
   if #toc > 2 then
-    io.write("### for (",toc[#toc - 1]:describe(),") ....\n")
+    io.write("### for (",toc[#toc - 1].name,") ....\n")
     io.write("{{",nav,'\n')
-    io.write("| 1 = ",toc[#toc-2]:short(),'\n')
-    io.write("| 2 = ",toc[#toc]:short(),'\n')
+    io.write("| 1 = ",toc[#toc-2].short, '\n')
+    io.write("| 2 = ",toc[#toc].short,'\n')
     io.write("}}\n\n")
-    io.write("&rarr; ",toc[#toc]:long()," &rarr;\n\n")
+    io.write("&rarr; ",toc[#toc].long," &rarr;\n\n")
   end
 end
 
 -- output the last one
-io.write("### for ",toc[#toc]:describe()," ....\n")
+io.write("### for ",toc[#toc].name," ....\n")
 io.write("{{",nav,'\n')
-io.write("| 1 = ",toc[#toc-1]:short(),'\n')
-io.write("| 2 = ",toc[1]:short(),'\n')
+io.write("| 1 = ",toc[#toc-1].short,'\n')
+io.write("| 2 = ",toc[1].short,'\n')
 io.write("}}\n\n")
 
-local spaced = toc[1]:long():gsub("^..:([^|]+)|Con.+","%1"):gsub(" ","_")
+local spaced = toc[1].long:gsub("^..:([^|]+)|Con.+","%1"):gsub(" ","_")
 io.write("~~~ the Table of contents is ", spaced, " ~~~\n")
-for i=2,#toc do io.write("* ",toc[i]:long(),'\n') end
+for i=2,#toc do io.write("* ",toc[i].long,'\n') end
 
 io.write("\n\nNav is Template:",nav:gsub(" ","_"),'\n')
 io.write(string.format([==[
