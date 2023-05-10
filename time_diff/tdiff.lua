863b872397d4eebef262c2484067109715d46455
Richard Todd <rwtodd@users.noreply.github.com>
2023-05-09 19:25:29 -0500

    updates to the error reporting and made the dash in the input mandatory

--- time_diff/tdiff.lua
+++ time_diff/tdiff.lua
@@ -7,15 +7,20 @@ local function normalize_time(h,m,ap)
   return ((h%12) + adder)*60 + m
 end
 
+local function report(day, minutes, class)
+  io.write("Day ",day,": ",math.floor(minutes/60.0),"h ",minutes%60,"m ", class, "\n")
+end
+
 io.write("input format:  HH:MMam - HH:MMpm MM\n")
 io.write("               start   - end     lunch\n")
 
 for l in io.lines() do
   if(l == "" or l:match("^#")) then goto CONTINUE end
   which_line = which_line + 1
-  local h1,m1,ap1,h2,m2,ap2,lunch = l:match("(%d+):(%d+)([ap])m?%s+%-?%s*(%d+):(%d+)([ap])m?%s+(%d+)")
+
+  local h1,m1,ap1,h2,m2,ap2,lunch = l:match("(%d+):(%d+)([ap])m?%s+%-%s+(%d+):(%d+)([ap])m?%s+(%d+)")
   if(not lunch) then
-    io.write("BAD INPUT:", l)
+    io.write("Line: ", which_line, " BAD INPUT:", l)
     break
   end
   io.write("\nDay ",which_line," input: ", l, "\n")
@@ -23,20 +28,20 @@ for l in io.lines() do
   local minutes = m2 - m1 - lunch
   local today = totalmins + minutes 
   if today <= forty_hours then
-    io.write("Day ",which_line,": ",math.floor(minutes/60.0),"h ",minutes%60,"m REG\n")
+    report(which_line, minutes, 'REG')
   elseif totalmins < forty_hours  then
     local remainder = forty_hours - totalmins
     local ot = minutes - remainder
-    io.write("Day ",which_line,": ",math.floor(remainder/60.0),"h ",remainder%60,"m REG\n")
-    io.write("  |=> ",math.floor(ot/60.0),"h ",ot%60,"m OT\n")
+    report(which_line, remainder, 'REG')
+    report(which_line, ot, 'OT')
   else
-    io.write("Day ",which_line,": ",math.floor(minutes/60.0),"h ",minutes%60,"m OT\n")
+    report(which_line, minutes, 'OT')
   end
   totalmins = today
   ::CONTINUE::
 end
 
 local rt,ot = math.min(totalmins, forty_hours), math.max(totalmins - forty_hours, 0)
-io.write("\nTOTALS: ",math.floor(rt/60.0),"h ",rt%60,"m REG\n")
-io.write("  |==> ",math.floor(ot/60.0),"h ",ot%60,"m OT\n")
-
+io.write('\n===========================================\n')
+report("TOTAL", rt, 'REG')
+report("TOTAL", ot, 'OT')
