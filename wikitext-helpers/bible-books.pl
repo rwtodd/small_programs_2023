52accae8da51e13f11c5a2361eccd66e9283e585
Richard Todd <rwtodd@users.noreply.github.com>
2023-06-20 17:27:38 -0500

    now outputting chapter text, basically working now

--- wikitext-helpers/bible-books.pl
+++ wikitext-helpers/bible-books.pl
@@ -2,9 +2,10 @@
 
 use v5.36;
 use XML::LibXML;
+use constant OUTDIR => 'out';
 
 # Here are the expanded names for the various bible versions I'm working on...
-my %expanded_names = (
+my %expanded_vnames = (
   NIV => 'New International Verison',
   NRSV => 'New Revised Standard Version',
   KJV => 'King James Version',
@@ -127,6 +128,88 @@ my %book_numbers = (
    'Jude' => 65,
    'Revelation' => 66);
 
+my %abbrev_book = (
+   'Genesis' => 'GEN',
+   'Exodus' => 'EXO',
+   'Leviticus' => 'LEV',
+   'Numbers' => 'NUM',
+   'Deuteronomy' => 'DEU',
+   'Joshua' => 'JOS',
+   'Judges' => 'JDG',
+   'Ruth' => 'RUT',
+   '1 Samuel' => '1SA',
+   '2 Samuel' => '2SA',
+   '1 Kings' => '1KI',
+   '2 Kings' => '2KI',
+   '1 Chronicles' => '1CH',
+   '2 Chronicles' => '2CH',
+   'Ezra' => 'EZR',
+   'Nehemiah' => 'NEH',
+   'Esther' => 'EST',
+   'Job' => 'JOB',
+   'Psalms' => 'PSA',
+   'Proverbs' => 'PRO',
+   'Ecclesiastes' => 'ECC',
+   'Song of Solomon' => 'SNG',
+   'Isaiah' => 'ISA',
+   'Jeremiah' => 'JER',
+   'Lamentations' => 'LAM',
+   'Ezekiel' => 'EZK',
+   'Daniel' => 'DAN',
+   'Hosea' => 'HOS',
+   'Joel' => 'JOL',
+   'Amos' => 'AMO',
+   'Obadiah' => 'OBA',
+   'Jonah' => 'JON',
+   'Micah' => 'MIC',
+   'Nahum' => 'NAM',
+   'Habakkuk' => 'HAB',
+   'Zephaniah' => 'ZEP',
+   'Haggai' => 'HAG',
+   'Zechariah' => 'ZEC',
+   'Malachi' => 'MAL',
+   'Tobit' => 'TOB',
+   'Judith' => 'JDT',
+   'Additions to Esther' => 'ADE',
+   'Wisdom' => 'WIS',
+   'Sirach' => 'SIR',
+   'Baruch' => 'BAR',
+   'Additions to Daniel' => 'ADD',
+   'Susanna' => 'SUS',
+   'Bel and the Dragon' => 'BEL',
+   '1 Maccabees' => '1MA',
+   '2 Maccabees' => '2MA',
+   '1 Esdras' => '1ES',
+   'Manassse' => 'MAN',
+   '2 Esdras' => '2ES',
+   'Matthew' => 'MAT',
+   'Mark' => 'MRK',
+   'Luke' => 'LUK',
+   'John' => 'JHN',
+   'Acts' => 'ACT',
+   'Romans' => 'ROM',
+   '1 Corinthians' => '1CO',
+   '2 Corinthians' => '2CO',
+   'Galatians' => 'GAL',
+   'Ephesians' => 'EPH',
+   'Philippians' => 'PHP',
+   'Colossians' => 'COL',
+   '1 Thessalonians' => '1TH',
+   '2 Thessalonians' => '2TH',
+   '1 Timothy' => '1TI',
+   '2 Timothy' => '2TI',
+   'Titus' => 'TIT',
+   'Philemon' => 'PHM',
+   'Hebrews' => 'HEB',
+   'James' => 'JAS',
+   '1 Peter' => '1PE',
+   '2 Peter' => '2PE',
+   '1 John' => '1JN',
+   '2 John' => '2JN',
+   '3 John' => '3JN',
+   'Jude' => 'JUD',
+   'Revelation' => 'REV');
+
 # make a number => name lookup as well
 my %book_names = reverse %book_numbers;
 
@@ -156,9 +239,9 @@ sub all_books_named_ok($bible) {
   return $result;
 }
 
-# determine the chapter's file name on disk, from the link text
-sub chapter_file_name($link) {
-  my $fname = "$link.wikitext";
+# determine the chapter's file name on disk, from the page name
+sub chapter_file_name($page_name) {
+  my $fname = "$page_name.wikitext";
   $fname =~ tr/ /_/;
   return $fname;
 }
@@ -172,17 +255,23 @@ sub determine_bible_version($xml_fn) {
 
 sub derive_chapter_from_page($page) {
   if($page =~ m/ (\d+) \([A-Z]/) {
-    return $1;
+    return $1 + 0;
   }
   die "could not determind chapter number from page <$page>";
+}
+
+#standardize how we make a table of contents link
+sub toc_page_name($version) {
+  my $exp = $expanded_vnames{$version} // "Holy Bible";
+  "$exp ($version)"
 }
 
 # output the table of contents for a bible...
 # $toc will be an array ref:
 #    [ [ "Book Name", link1, link2, ... ], [ "Book Name" ... ] ]
 sub output_toc($version, $toc) {
-  my $exp = $expanded_names{$version} // "Holy Bible";
-  my $toc_fn = "$exp ($version).wikitext";
+  my $toc_page = toc_page_name($version);
+  my $toc_fn = OUTDIR . "/$toc_page.wikitext";
   $toc_fn =~ tr/ /_/;
   open my $fh, '>:utf8', $toc_fn or die "Could not open TOC file <$toc_fn>!";
 
@@ -206,15 +295,85 @@ sub output_toc($version, $toc) {
   close $fh;
 }
 
-# $chap is XML node, and $link is the name of the wiki page for the chapter
-sub output_chapter($chap, $link) {  
-  my $outfn = chapter_file_name($link);
-  say "outputting <$outfn>";
+# Standardize how we format links to a book's chapters...
+sub chapter_page_name($version, $bookname, $chap_node) {
+  my $cnum = $chap_node->findvalue('@cnumber'); 
+  "$bookname $cnum ($version)" 
+}
+
+sub chapter_link($version, $bookname, $chap_node) {
+  my $cnum = $chap_node->findvalue('@cnumber'); 
+  my $short = $abbrev_book{$bookname};
+  "[[$bookname $cnum ($version)|$short $cnum]]" 
+}
+
+sub prev_chapter_link($version, $book, $chap_node) {
+  my $pc = $chap_node->previousNonBlankSibling();
+  return chapter_link($version, $book, $pc) if (defined $pc);
+
+  # ok, we were the first chapter, so we've got to look up the
+  # last chapter of the previous book
+  my $pbook = $chap_node->parentNode->previousNonBlankSibling();
+  if(defined $pbook) {
+    my $pbname = book_name($pbook);
+    my $lastChap = $pbook->find('./CHAPTER[last()]')->[0];
+    return chapter_link($version, $pbname, $lastChap);
+  }
+
+  # if there was no previous book, just return the TOC page...
+  my $toc_page = toc_page_name($version);
+  return "[[$toc_page|Contents]]";
+}
+
+sub next_chapter_link($version, $book, $chap_node) {
+  my $nc = $chap_node->nextNonBlankSibling();
+  return chapter_link($version, $book, $nc) if (defined $nc);
+
+  # ok, we were the last chapter, so we've got to look up the
+  # first chapter of the next book
+  my $nbook = $chap_node->parentNode->nextNonBlankSibling();
+  if(defined $nbook) {
+    my $nbname = book_name($nbook);
+    my $firstChap = $nbook->find('./CHAPTER[1]')->[0];
+    return chapter_link($version, $nbname, $firstChap);
+  }
+
+  # if there was no next book, just return the TOC page...
+  my $toc_page = toc_page_name($version);
+  return "[[$toc_page|Contents]]";
+}
+
+sub output_chapter($version, $book, $chap_node) {  
+  my $cur_page = chapter_page_name($version, $book, $chap_node);
+  my $cur_link = chapter_link($version, $book, $chap_node);
+  my $prev_link = prev_chapter_link($version, $book, $chap_node); 
+  my $next_link = next_chapter_link($version, $book, $chap_node); 
+  my $outfn = chapter_file_name($cur_page);
+  my $toc_page = toc_page_name($version);
+  $cur_page =~ s/ *\(.*\)$//; # remove the version info from the page
+  open my $fh, '>:utf8', (OUTDIR . "/$outfn");
+  $fh->say(<<~NAV);
+  {{Bibe Nav
+  |1=$toc_page
+  |2=$cur_page
+  |3=$prev_link
+  |4=$next_link}}
+  NAV
+  my $count = 1;
+  for my $verse ($chap_node->findnodes('VERS')) {
+    my $vnum = $verse->findvalue('@vnumber') + 0;
+    warn "missing verse? count of <$count> vs <$vnum>" unless $vnum == $count;
+    my $txt = $verse->to_literal();
+    $fh->print("<span id=\"V$vnum\">'''$vnum.'''</span> ",$txt,"\n\n");
+  } continue { $count++ }
+  $fh->say("&rarr; $next_link &rarr;");
+  $fh->say("[[Category:Bible Text ($version)]]");
+  close $fh;
 }
 
 sub process_bible($xml_fn) {
-  my $ver = determine_bible_version($xml_fn);
-  say "Bible version is $ver";
+  my $version = determine_bible_version($xml_fn);
+  say "Bible version is $version";
 
   my @toc = ();  # build a table of contents as we go...
 
@@ -225,17 +384,17 @@ sub process_bible($xml_fn) {
     my $name = book_name($bk);
     my @chaps = ($name);
     for my $chap ($bk->findnodes('CHAPTER')) {
-      my $cnum = $chap->findvalue('@cnumber');
-      my $link = "$name $cnum ($ver)";
-      push @chaps, $link;
-      output_chapter($chap, $link);
+      push @chaps, chapter_page_name($name, $version, $chap);
+      output_chapter($version, $name, $chap);
     }
     push @toc, \@chaps;
+    last; # RWT TEMP!!!
   }
 
-  output_toc($ver,\@toc);
+  output_toc($version,\@toc);
 }
 
+mkdir OUTDIR unless -d OUTDIR;
 process_bible(shift or die "Usage; bible-books.pl XMLFILE");
 
 __END__
