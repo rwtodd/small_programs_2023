ccfd28670b70a22670b52726c223523a5ad55d19
Richard Todd <rwtodd@users.noreply.github.com>
2023-07-10 12:33:51 -0500

    learned about ruby ARGF

--- wikitext-helpers/lyrics_stanza.rb
+++ wikitext-helpers/lyrics_stanza.rb
@@ -6,13 +6,15 @@ def out_buffer()
 end
 
 puts "; Release Date: xxxx-xx-xx\n\n== Lyrics ==";
-STDIN.each_line do |l|
+ARGF.each_line do |l|
   l.strip!.sub!(%r{^\[(.*?)\]$},%q![''\1'']!)        # convert [xxxx] to [''xxx'']
   if @buffer.empty? and l.match(%r{^\d+\.\s+}) then  # is it a song title?
     puts "=== #{$'} ==="
-    next
+  elsif l.empty? then
+    out_buffer()
+  else
+    @buffer << l
   end 
-  if l.empty? then out_buffer() else @buffer << l end
 end
 out_buffer()
 puts "\n[[Category:Albums (Extreme Metal)]]";
