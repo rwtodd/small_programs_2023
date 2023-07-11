222470769ea5766293b10d4a4ffd321def094ab4
Richard Todd <rwtodd@users.noreply.github.com>
2023-07-10 21:14:29 -0500

    Moving toward ruby for these scripts
    I replaced the nextprev variants with a ruby version called
    `wb-toc`.  It is a library and driver, so that it can be
    used programmatically in larger projects (which might derive
    a TOC from the pages it is parsing, or might want to automatically
    add the wrappers to the content it is producing)

---
+++ wikitext-helpers/include/toc.rb
@@ -0,0 +1,187 @@
+# table of contents / nav pages for wikibooks
+#
+class WikiPage
+  @@no_spaces = /\s+/
+  @@last_parenthetical = %r!\s+ \(  [^)]+  \)  $!x
+
+  def initialize(pagename)
+    @pagename = pagename
+  end
+
+  def url
+    @pagename.gsub(@@no_spaces,'_')
+  end
+
+  def make_link(text=nil)
+    text == nil ? "[[#{@pagename}]]" : "[[#{@pagename}|#{text}]]"
+  end
+
+  attr_reader :pagename
+end
+
+class TocEntry < WikiPage
+  def initialize(pagename, short: nil, display: nil) 
+    super(pagename)
+    long = @pagename.sub(@@last_parenthetical, '')
+    @long = self.make_link(long)
+
+    short ||= long
+    if short.length > 20 then
+      short.sub!(%r!^(?:An?|The)\s+!,'')
+      if short.length > 20 then
+        short[18..] = '&hellip;'
+      end
+    end
+    @short = self.make_link(short)
+
+    if not display then
+      @display = "* #{@long}"
+    else
+      level = '*'
+      display.sub!(/^[#*;]* */) do |lvl| 
+        lvl.strip!
+        level = lvl.empty? ? '*' : lvl
+        ''
+      end
+      if display.empty? then display = long end   # default to the whole text
+      @display = "#{level} #{self.make_link(display)}"
+    end
+  end
+
+  attr_reader :long, :short, :display
+end
+
+# Represents the book's category (doubles as its table of contents)
+class BookCategory < WikiPage
+  def initialize(book)
+    super(":Category:#{book.pagename}")
+  end
+
+  def mark
+    self.make_link.sub!(':','')
+  end
+
+  def short; self.make_link('Contents'); end
+  def long; self.make_link('Table of Contents'); end
+end
+
+class TocData
+  def initialize(author, full_title, short_title=nil)
+    @short_title = short_title || full_title
+    @book = WikiPage.new("#{full_title} (#{author})")
+    @nav = WikiPage.new(@book.pagename + " Nav");
+    @category = BookCategory.new(@book)
+    @pages = []
+  end
+
+  def add_page(pagename, short: nil, display: nil) 
+    pages << TocEntry.new("#{pagename} (#{@short_title})", short: short, display:display)
+  end
+
+  def page_or_toc(n)
+    (n < 0 or n >= @pages.length) ? @category : @pages[n]
+  end
+
+  def page_header(n)
+    prv, cur, nxt = self.page_or_toc(n-1), self.page_or_toc(n), self.page_or_toc(n+1)
+    <<~ENTRY
+      {{#{@nav.pagename}
+      | 1 = #{prv.short}
+      | 2 = #{nxt.short}
+      }}
+    ENTRY
+  end 
+
+  def page_footer(n)
+    nxt = self.page_or_toc(n+1)
+    "&rarr; #{nxt.long} &rarr;"
+  end
+
+  def puts_page_wrappers
+    @pages.each_index do |n|
+      puts "\n\n### for <#{self.page_or_toc(n).pagename}> #########################", page_header(n), "", page_footer(n)
+    end
+    nil
+  end
+
+  def puts_toc_page()
+    unders = @book.url
+    puts "TOC page is Category:#{unders}", ""
+    puts "; Title: ???", "; Author: ????", "; Date: ????-??-??"
+    puts '' 
+    puts "[[File:#{unders}_CoverImage.jpg|thumb|Cover Image]]", "== Contents =="
+    @pages.each {|p| puts p.display }
+    nil
+  end
+
+  def puts_nav_page()
+    puts <<~NAVEND
+     Nav page is Template:#{@nav.url}
+
+     {| class="infobox wikitable floatright"
+     |-
+     ! scope="colgroup" colspan="2" | #{@full_title}
+     |-
+     | style="text-align:center" colspan="2" | #{@category.long}
+     |-
+     | style="text-align:left" | &larr;&nbsp;{{{1}}}
+     | style="text-align:right" | {{{2}}}&nbsp;&rarr;
+     |}<includeonly>
+     #{@category.mark}
+     </includeonly>
+     
+     NAVEND
+  end
+
+  attr_reader :pages
+
+  class << self
+    def from_io(io)
+      config = {full_title: nil,  short_title: nil, author: nil}
+
+      # first, read the config data up to '----'
+      io.each_line do |line|
+        line.strip!
+        break if line.start_with?('---')
+        next if line.empty? or line.start_with?('#')
+
+        k,v = line.split('=>',2)
+        k.strip!; k.gsub!(' ','_')
+        k = k.to_sym
+        if config.has_key?(k) then
+          if v then 
+            v.strip!
+            config[k] = v
+          else
+            STDERR.puts "Warning, no value given for key <#{k}>!"
+          end
+        else
+          STDERR.puts "Warning, unknown key <#{k}>!"
+        end
+      end
+      td = TocData.new(config[:author] || "unknown!", config[:full_title] || 'unknown!', config[:short_title])
+
+      # now, read all the TOC entries...
+      io.each_line do |line|
+        line.strip!
+        next if line.empty? or line.start_with?('#')
+
+        # format of a line:  page name [@toc [**] Toc Version] [@sh Short Version]
+        toc_part = nil; short_part = nil
+        line.sub!(/ @toc \s+ ([^@]+) /x) do |toc|
+          toc_part = $1.strip
+          ''
+        end
+        line.sub!(/ @sh \s+ ([^@]+) /x) do |shorter|
+          short_part = $1.strip
+          ''
+        end
+        line.strip!
+        td.add_page(line, short: short_part, display: toc_part)
+      end
+      td
+    end
+  end
+end
+
+# vim: sw=2 expandtab
