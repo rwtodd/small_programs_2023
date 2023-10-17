9ee3e9d9d0d84f573b055fbd6b02303639d776b2
Richard Todd <rwtodd@users.noreply.github.com>
2023-10-17 15:15:47 -0500

    added color text

--- video_cvt/convert-mp4s.sh
+++ video_cvt/convert-mp4s.sh
@@ -7,8 +7,16 @@
 # Just copy the audio as-is.  If the video is already 
 # hevc, just copy the video and retag it.  If the 
 # video is larger than 720p, then downscale it to 720p.
+#
+txtyellow=$(tput bold)$(tput setaf 3)
+txtreset=$(tput sgr0)
 DownTo720="no"
 CopyVideo="no"
+
+# print text in yellow
+function yecho() {
+   echo -e "$txtyellow$1$txtreset"
+}
 
 # collectinfo <filename>
 function collectinfo() {
@@ -20,15 +28,15 @@ function collectinfo() {
     case $line in
       codec_name=*)
         codec=${line#*=}
-	echo "Codec is <$codec>"
+	yecho "Codec is <$codec>"
 	if [ "$codec" == "hevc" ]; then CopyVideo="yes"; fi
 	;;
       height=*)
 	height=${line#*=}
-	echo "Height is <$height>"
+	yecho "Height is <$height>"
 	if (( height > 720 )); then DownTo720="yes"; fi
 	;;
-      *) echo "unknown line $line"
+      *) yecho "unknown line <$line>"
     esac 
   done
 }
@@ -36,22 +44,22 @@ function collectinfo() {
 while [ $# -gt 0 ]; do
   infile=$1; shift
   echo ""
-  echo "********************************************************"
+  yecho "********************************************************"
   collectinfo $infile
   converted=$(basename ${infile%.*}).mp4
-  echo "Going to write output <$converted>"
+  yecho "Going to write output <$converted>"
   if [ -f $converted ] ; then
-    echo "$converted already exists!"
+    yecho "$converted already exists!"
     continue
   fi
   echo "CopyVideo = $CopyVideo"
   echo "DownTo720 = $DownTo720"
 
   if [ $DownTo720 == yes ]; then
-    echo "Downscaling to 720p."
+    yecho "Downscaling to 720p."
     ffmpeg -i $infile -vf scale=-1:720 -sn -c:v libx265 -crf 24 -tag:v hvc1 -c:a copy $converted
   elif [ $CopyVideo == yes ]; then
-    echo "Copying video (not re-encoding)"
+    yecho "Copying video (not re-encoding)"
     ffmpeg -i $infile -sn -c:v copy -crf 25 -tag:v hvc1 -c:a copy $converted
   else
     ffmpeg -i $infile -sn -c:v libx265 -crf 25 -tag:v hvc1 -c:a copy $converted
