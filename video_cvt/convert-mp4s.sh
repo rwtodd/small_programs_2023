c7d7c342ff9ce3ec94f66fef89a96ee560a38780
Richard Todd <rwtodd@users.noreply.github.com>
2023-10-17 19:31:49 -0500

    added more options to the ffmpeg wrapper

--- video_cvt/convert-mp4s.sh
+++ video_cvt/convert-mp4s.sh
@@ -11,7 +11,9 @@
 txtyellow=$(tput bold)$(tput setaf 3)
 txtreset=$(tput sgr0)
 DownTo720="no"
-CopyVideo="no"
+CopyHEVC="no"
+crflevel=25
+extargs=""
 
 # print text in yellow
 function yecho() {
@@ -23,28 +25,42 @@ function collectinfo() {
   local codec=''
   local height=0
   ffprobe -i $1 -select_streams v:0 -show_entries stream="height,codec_name" \
-	  -of default=noprint_wrappers=1:nokey=0 -v error | \
-	  while read line; do 
+          -of default=noprint_wrappers=1:nokey=0 -v error | \
+          while read line; do 
     case $line in
       codec_name=*)
         codec=${line#*=}
-	yecho "Codec is <$codec>"
-	if [ "$codec" == "hevc" ]; then CopyVideo="yes"; fi
-	;;
+        yecho "Codec is <$codec>"
+        if [ "$codec" == "hevc" ]; then CopyHEVC="yes"; fi
+        ;;
       height=*)
-	height=${line#*=}
-	yecho "Height is <$height>"
-	if (( height > 720 )); then DownTo720="yes"; fi
-	;;
+        height=${line#*=}
+        yecho "Height is <$height>"
+        if (( height > 720 )); then DownTo720="yes"; fi
+        ;;
       *) yecho "unknown line <$line>"
     esac 
   done
 }
 
+# check for input arguments
+while getopts "e:c:h" o; do
+  case "${o}" in
+    c) crflevel=$OPTARG
+       ;;
+    e) extargs="$OPTARG"
+       ;;
+    h) yecho "Usage: $0 [-c <crf>] [-e <extra ffmpeg args>] filenames..."
+       exit 1
+       ;;       
+  esac
+done
+shift $((OPTIND - 1))
+
 while [ $# -gt 0 ]; do
   infile=$1; shift
   echo ""
-  yecho "********************************************************"
+  yecho "************************************************************"
   collectinfo $infile
   converted=$(basename ${infile%.*}).mp4
   yecho "Going to write output <$converted>"
@@ -52,17 +68,22 @@ while [ $# -gt 0 ]; do
     yecho "$converted already exists!"
     continue
   fi
-  echo "CopyVideo = $CopyVideo"
-  echo "DownTo720 = $DownTo720"
+  echo "CopyHEVC   =  $CopyHEVC"
+  echo "DownTo720  =  $DownTo720"
+  echo "CRF Level  =  $crflevel"
+  if [ "$extargs" != "" ]; then
+    echo "Extra args =  <$extargs>"
+  fi
 
   if [ $DownTo720 == yes ]; then
-    yecho "Downscaling to 720p."
-    ffmpeg -i $infile -vf scale=-1:720 -sn -c:v libx265 -crf 24 -tag:v hvc1 -c:a copy $converted
-  elif [ $CopyVideo == yes ]; then
-    yecho "Copying video (not re-encoding)"
-    ffmpeg -i $infile -sn -c:v copy -crf 25 -tag:v hvc1 -c:a copy $converted
+    crflevel=$((crflevel - 1))
+    yecho "Downscaling to 720p. CRF improved to $crflevel"
+    ffmpeg -i $infile -vf scale=-1:720 -sn -c:v libx265 -crf $crflevel -tag:v hvc1 -c:a copy $extargs $converted
+  elif [ $CopyHEVC == yes ]; then
+    yecho "Copying HEVC video (not re-encoding)"
+    ffmpeg -i $infile -sn -c:v copy -tag:v hvc1 -c:a copy $extargs $converted
   else
-    ffmpeg -i $infile -sn -c:v libx265 -crf 25 -tag:v hvc1 -c:a copy $converted
+    ffmpeg -i $infile -sn -c:v libx265 -crf $crflevel -tag:v hvc1 -c:a copy $extargs $converted
   fi
 done
 
