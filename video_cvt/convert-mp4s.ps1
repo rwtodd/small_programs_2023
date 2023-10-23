6f586b23f8ed917254ff76f3c2aad6b440811fe0
Richard Todd <rwtodd@users.noreply.github.com>
2023-10-23 01:52:13 -0500

    made the powershell version more like the perl one

--- video_cvt/convert-mp4s.ps1
+++ video_cvt/convert-mp4s.ps1
@@ -6,8 +6,27 @@ than 720p, then downscale it to 720p.
 [CmdletBinding(SupportsShouldProcess)]
 param(
   [Parameter(ValueFromPipeline)]
-  [string[]]$infiles
+  [string[]]$infiles,
+  [switch]$Status,
+  [int]$CRF = 25
 )
+BEGIN {
+  $seeStatus = @()
+  $quietMode = ('-hide_banner', '-loglevel', 'error')
+  $x265parms = @('-x265-params', 'log-level=error')
+  $audioparms = @('-c:a', 'copy')
+  $chapts = @('-dn', '-map_chapters', '-1')
+  $extargs = @()
+
+  if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+    $quietMode = @()
+    $x265parms = @()
+  }
+  if ($Status -or $PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+    $seeStatus = @('-stats')
+  }
+}
+
 PROCESS {
 
   foreach ($fl in $infiles) {
@@ -17,7 +36,8 @@ PROCESS {
     }
 
     $resolved = get-item -LiteralPath $fl
-    $converted = ($resolved.Basename -replace '[]['' _-]+','_') + ".mp4"
+    $converted = ($resolved.Basename -replace '[^a-zA-Z0-9]+','_') + ".mp4"
+
     Write-Verbose "Converting to output name: <$converted>"
     if (test-path $converted) {
       Write-Error "$converted already exists!"
@@ -27,35 +47,41 @@ PROCESS {
     # run ffprobe to figure out what to do...
     $CopyVideo = $false
     $DownTo720 = $false
-    switch -Regex (&ffprobe -i $resolved  -select_streams v:0 -show_entries stream="height,codec_name" -of default=noprint_wrappers=1:nokey=0 -v error) {
-      '^codec_name=(.*)' { 
+    switch -Regex (&ffprobe -i $resolved -select_streams v:0 -show_entries stream="height,codec_name" -of default=noprint_wrappers=1:nokey=0 -v error) {
+      '^codec_name=(.*)' {
         Write-Verbose "Codec = $($matches[1])"
-        if ($matches[1] -eq 'hevc') {
-          $CopyVideo = $true;
-        }
+        $CopyVideo = $matches[1] -eq 'hevc'
       }
       '^height=(.*)' {
         Write-Verbose "Height = $($matches[1])"
-        if ([int]($matches[1]) -gt 720) {
-          $DownTo720 = $true;
-        }
+        $DownTo720 = [int]($matches[1]) -gt 720
       }
     }
+    if($DownTo720) { $CopyVideo = $false } # can't copy AND downscale!
     Write-Verbose "`$DownTo720 == $DownTo720"
     Write-Verbose "`$CopyVideo == $CopyVideo"
-      
+    Write-Verbose "`$CRF       == $CRF"  
+
     if ($PSCmdlet.ShouldProcess("$resolved", "Convert Video")) {
+      $thisCRF = $CRF
+      $cmd = @('ffmpeg') + $quietMode + $seeStatus + @('-i', "`"{0}`"" -f [WildcardPattern]::Escape($resolved), '-sn')
       if ($DownTo720) {
-        Write-Verbose "Downscaling to 720p."
-        ffmpeg -i $resolved -vf scale=-1:720 -sn -c:v libx265 -crf 24 -tag:v hvc1 -c:a copy $converted
+	$cmd += ('-vf','scale=-1:720')
+	$thisCRF = $thisCRF - 1
+        Write-Verbose "Downscaling to 720p, lowering CRF to $thisCRF"
       }
-      elseif ($CopyVideo) {
-        Write-Verbose "Copying video rather than converting."
-        ffmpeg -i $resolved -sn -c:v copy -tag:v hvc1 -c:a copy $converted
+
+      if ($CopyVideo) {
+        Write-Verbose 'Copying HEVC video (not re-encoding)'
+        $cmd += ('-c:v','copy')
+      } else {
+        $cmd += ('-c:v','libx265','-crf',$thisCRF)
       }
-      else {
-        ffmpeg -i $resolved -sn -c:v libx265 -crf 25 -tag:v hvc1 -c:a copy $converted
-      }
+
+      $cmd += @('-tag:v', 'hvc1') + $x265parms + $audioparms + $chapts + $extargs
+      $cmd += @($converted)
+      Write-Verbose "Command: $cmd"
+      Invoke-Expression ($cmd -join ' ')
     }
   }
 }
