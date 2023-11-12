c0d5f52d825a58acfc05b3706b00807707f3ca44
Richard Todd <rwtodd@users.noreply.github.com>
2023-11-12 15:15:13 -0600

    tweaking the binfind programs

--- binfind/binfind.lua
+++ binfind/binfind.lua
@@ -6,24 +6,23 @@ local function hexencode(str)
    return (str:gsub(".", function(char) return string.format(" %02x", char:byte()) end))
 end
 
-if #arg < 2 then
-  print("bad arguments!")
+if #arg < 2 or #arg[1] % 2 == 1 or not string.match(arg[1], '^%x+$') then
+  io.stderr:write("Usage: binfind.lua <hex-seq> <filenames>\n")
   os.exit(1)
 end
 
-local tofind = hexdecode(arg[1])
-local context = math.max(0, (16 - #tofind)//2)
+local tofind <const> = hexdecode(arg[1])
+local context <const> = math.max(0, (16 - #tofind)//2)
 
 for argnum = 2,#arg do
-  local f = assert(io.open(arg[argnum], "rb"))
-  local content = f:read("*all")
-  f:close()
+  local f <close> = assert(io.open(arg[argnum], "rb"))
+  local content <const> = f:read("*all")
   local loc,pos2 = 1, 1
   while loc do
     loc, pos2 = content:find(tofind,loc,1)
     if loc then
-      local index = math.max(1,loc - context)
-      io.write(string.format("%s %08X:%s\n", arg[argnum], loc - 1, hexencode(content:sub(index,index+16))))
+      local index <const> = math.max(1,loc - context)
+      io.write(("%s: %08X:%s\n"):format(arg[argnum], loc - 1, hexencode(content:sub(index,index+15))))
       loc = pos2+1
     end
   end
--- binfind/binfind.pl
+++ binfind/binfind.pl
@@ -5,18 +5,15 @@ use File::Basename qw/basename/;
 use List::Util qw/max/;
 use autodie;
 
-my $hex_seq = shift or die "Usage: binfind.pl <hex-seq> <filenames>";
-die "hex string must by multiple-of-2 length!" unless length($hex_seq) % 2 == 0;
+my $hex_seq = shift or die "Usage: binfind.pl <hex-string> <filenames>";
+die "invalid hex string!" unless $hex_seq =~ /^(?:[[:xdigit:]]{2})+$/;
 $hex_seq = pack "H*", $hex_seq;
 my $hex_rx = qr/\Q$hex_seq\E/;
 my $context = max(0, int((16 - length($hex_seq))/2)); # number of bytes of context...
 
-my $buffer;
 while(my $fn = shift) {
-  open my $infl, '<:raw', $fn;
   my $bn = basename($fn);
-  $infl->read($buffer, -s $fn);
-  $infl->close;
+  my $buffer = do { open my $infl, '<:raw', $fn; local $/; <$infl> };
 
   # search the buffer
   local $, = ' '; # for printing the unpacked bytes
