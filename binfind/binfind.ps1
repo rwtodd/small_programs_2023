54eeaadd33fa3e36751bd77b1e9c342e300f67d0
Richard Todd <rwtodd@users.noreply.github.com>
2023-11-14 23:02:40 -0600

    fix binfind.ps1 to work with paths that have special chars, and write to pipeline

--- binfind/binfind.ps1
+++ binfind/binfind.ps1
@@ -1,6 +1,3 @@
-<#
-Find a binary sequence inside the raw bytes of a file.
-#>
 [CmdletBinding()]
 param(
     [Parameter(Position=0,Mandatory)]
@@ -17,13 +14,14 @@ BEGIN {
 }
 PROCESS {
     foreach($fl in $InFiles) {
-        $resolved = get-item -Path $fl
-        [String]$fileBytes = get-content -path $fl -Encoding $encoder -Raw
+        $resolved = get-item -LiteralPath $fl
+        Write-Verbose "Searching $fl"
+        [String]$fileBytes = get-content -LiteralPath $resolved.FullName -Encoding $encoder -Raw
         foreach($match in $matcher.Matches($fileBytes)) {
             $idx = [Math]::max(0, $match.Index - $context)
             $sstr = [Convert]::ToHexString( $encoder.GetBytes($fileBytes.Substring($idx, 16)) ) `
                -replace '..',' $0'
-            Write-Host ("{0}: {1:X8}:{2}" -f $resolved.Name,$match.Index,$sstr)
+            Write-Output ("{0}: {1:X8}:{2}" -f $resolved.Name,$match.Index,$sstr)
             $found = $true
         }
     }
@@ -31,3 +29,25 @@ PROCESS {
 END {
     exit ($found ? 0 : 1)
 }
+
+<#
+.SYNOPSIS
+Finds a sequence of bytes, given in hex, in a set of files.
+
+.DESCRIPTION
+It uses a regex search of the files read in as raw strings.  This 
+script is not suitable for searching huge files, as the input
+files are read into memory in their entirety.
+
+.PARAMETER BinSeq
+A hex string representing the sequence of bytes for which to search.
+It must be a concatenated set of 2-digit hex codes, (e.g., 'cd100aff').
+
+.PARAMETER InFiles
+A list of files to search. This file list can come from the pipeline.
+
+.EXAMPLE
+
+PS> gci *.exe | binfind.ps1 cd21 # search for INT 21h in DOS .EXEs
+ 
+#>
